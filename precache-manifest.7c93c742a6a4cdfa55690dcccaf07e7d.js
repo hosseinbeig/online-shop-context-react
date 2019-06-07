3c36432e8961000368ed9392c33d094ec7f5020a
hosseinbeig <h.beig@yahoo.com>
2019-06-07 14:39:15 +0800

    Updates

---
+++ precache-manifest.7c93c742a6a4cdfa55690dcccaf07e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d409ed0784e8be2eb3ee0e0209947625",
+    "url": "/online-shop-context-react/index.html"
+  },
+  {
+    "revision": "13083f75eee71960249e",
+    "url": "/online-shop-context-react/static/css/main.4c2a57bc.chunk.css"
+  },
+  {
+    "revision": "94943e8854db55d980ff",
+    "url": "/online-shop-context-react/static/js/2.a67a8cc1.chunk.js"
+  },
+  {
+    "revision": "13083f75eee71960249e",
+    "url": "/online-shop-context-react/static/js/main.46869967.chunk.js"
+  },
+  {
+    "revision": "f49697e0aa8815685c63",
+    "url": "/online-shop-context-react/static/js/runtime~main.96ceee0f.js"
+  }
+]);
